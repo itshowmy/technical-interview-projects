0ea1047a38ad8cac7c5ec81ef26e53374df01787
Showmyo  Costa <Showmy@MacBook-Air-de-Showmyo.local>
2023-10-26 12:11:16 +0200

    added project 02 and completed first steps

--- 01-project-react-randomuser/src/App.tsx
+++ 01-project-react-randomuser/src/App.tsx
@@ -1,10 +1,27 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App () {
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    fetch('https://randomuser.me/api/?results=100')
+      .then(async res => await res.json())
+      .then(res => {
+        setUsers(res.results)
+      })
+      .catch(err => {
+        console.error(err)
+      })
+  }, [])
+
   return (
-    <>
+    <div className='App'>
       <h1>Technical interview project with TypeScript and React</h1>
-    </>
+      {
+        JSON.stringify(users)
+      }
+    </div>
   )
 }
 
